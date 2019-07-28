7ad03ac450b28b7628a5c216e85ed607fddc4dfb
Ben Steiger <rkrkakak456@gmail.com>
2019-07-28 16:19:05 +0900

    원하는 크기 이미지검색 구현?함
    이렇게 좋고 편한 url이 있는걸 왜 이제야 알았을까

--- dd.py
+++ dd.py
@@ -5,38 +5,46 @@ import os
 import urllib.request
 import multiprocessing
 
-#검색 url생성 searchterm 찾을 이미지
+#검색 url생성 searchterm 찾을 이미지 / 이미지 가로 / 이미지 세로크기
 searchterm = 'megadeth'
-url = "https://www.google.com/search?q="+searchterm+"&source=lnms&tbm=isch"
+width = '1280'
+height = '720'
+''' image size graph
+SD size 720 576
+HD size 1280 720
+FULL HD 1920 1080
+Quad HD 3840 2160
+2K 4K 4096 2160
+
+<div class="oW2d8b">
+width
+<input jsname="d7w5xc" class="ktf mini" value="" autocomplete="off" id="kbqVne" type="text">
+height
+<input jsname="zPIbne" class="ktf mini" value="" autocomplete="off" id="XOyNzd" type="text">
+'''
+#단순 이미지 검색
+#url = "https://www.google.com/search?q="+searchterm+"&source=lnms&tbm=isch"
+#이미지 크기선택 검색
+url = "https://www.google.com/search?q="+searchterm+"&biw=1662&bih=784&tbm=isch&source=lnt&tbs=isz:ex,iszw:"+horizontal+",iszh:"+vertical
+
 # chrom webdriver 사용하여 브라우저를 가져온다.
 browser = webdriver.Chrome('./chromedriver_win32-1/chromedriver')
 browser.get(url)
+
 # User-Agent를 통해 봇이 아닌 유저정보라는 것을 위해 사용
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 # 이미지 카운터
 counter = 0
 succounter = 0
 
-#image size HOR//VER
-horizontal = 1280
-vertical = 720
-'''
-SD size 720 576
-HD size 1280 720
-FULL HD 1920 1080
-Quad HD 3840 2160
-2K 4K 4096 2160
-Horizenal
-<input jsname="d7w5xc" class="ktf mini" value="" autocomplete="off" id="kbqVne" type="text">
-Virtualize
-<input jsname="zPIbne" class="ktf mini" value="" autocomplete="off" id="XOyNzd" type="text">
-'''
-
 print(os.path)
 # 소스코드가 있는 경로에 '검색어' 폴더없으면 생성
 if not os.path.exists(searchterm):
     os.mkdir(searchterm)
-   
+'''
+browser.find_element_by_xpath('//input 
+
+'''
 for _ in range(500):
     # 가로 = 0, 세로 = 10000 픽셀 스크롤한다.
     browser.execute_script("window.scrollBy(0,10000)")
@@ -63,10 +71,6 @@ for x in browser.find_elements_by_xpath('//div[contains(@class,"rg_meta")]'):
         File.write(raw_img)
         File.close()
         succounter = succounter + 1
-        '''
-        python3 버전경우 urllib.request고
-        python2 일 경우 urllib2임
-        '''
     except:
         print ("can't get img")
             
